b7002a509d161b688c8767d4ac8eb4274ce97a1a
danishfurniture <47069340+danishfurniture@users.noreply.github.com>
2019-06-04 21:42:41 -0500

    Add files via upload

---
+++ QB_Nebulae_V2/Code/nebulae/calibration_collector.py
@@ -0,0 +1,43 @@
+import os
+import time
+import controlhandler
+
+class CalibrationCollector(object):
+    def __init__(self):
+        self.ch = controlhandler.ControlHandler(None, 0, None) # Create new Controlhandler.
+
+    def collect(self):
+        self.ch.updateAll()
+        #data = []
+        avg = 0
+        numSamps = 4096
+        names = ['start', 'size', 'density', 'overlap', 'blend', 'window', 'speed', 'pitch']
+        avgs = {}
+        for name in names:
+            avgs[name] = 0.0
+        for i in range(0, numSamps):
+            self.ch.updateAll()
+            for name in names:
+                val = self.ch.channeldict[name].getRawCVValue()
+                avgs[name] += val
+        for name in names:
+            avgs[name] = avgs[name] / numSamps
+        filepath = '/home/alarm/QB_Nebulae_V2/Code/misc/'
+        filename = 'calibration_data.txt'
+        os.system("sh /home/alarm/QB_Nebulae_V2/Code/scripts/mountfs.sh rw")
+        with open(filepath + filename, 'w') as myfile:
+            for name in names:
+                stuff = [name, str(avgs[name]), '\n']
+                line = ','.join(stuff)
+                myfile.write(line)
+        os.system("sh /home/alarm/QB_Nebulae_V2/Code/scripts/mountfs.sh ro")
+
+######## begin print calibration data (danishfurniture)
+
+	f = open(filepath + filename)
+	calibration = f.readlines()
+	f.close()
+	for i in range(0, len(calibration)):
+		print calibration[i]
+
+######## end print calibration data (danishfurniture)
